1acd4919ee21946dad6e76ec59f79f1ede5411c5
Fernando Ballesteros santamaria <fballest@c1r2s4.42madrid.com>
2021-09-16 10:23:42 +0200

    normi

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fballest <fballest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 14:07:06 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/09/15 10:07:27 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/09/16 10:23:19 by fballest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ typedef enum s_type
 	APPEND
 }			t_type;
 
-typedef struct	s_env
+typedef struct s_env
 {
 	char	**envp;
 	char	*home;
@@ -49,22 +49,19 @@ typedef struct s_node
 
 typedef struct s_cmd_list
 {
-	t_node	*content;
-	struct s_cmd_list *next;
+	t_node				*content;
+	struct s_cmd_list	*next;
 }	t_cmd_list;
 
-
-
-
 //minishell.h
-int		main(int argc, char **argv, char **envp);
+int			main(int argc, char **argv, char **envp);
 
 //environments.h
-t_env	*take_envs(char	**envp);
+t_env		*take_envs(char	**envp);
 
 //parse.c
 t_cmd_list	*parse(char *prompt, t_env *env);
-void	add_word_to_list(char *word, t_cmd_list *command_line);
-void	ft_cmdlstadd_back(t_cmd_list *alst, t_node *new);
+void		add_word_to_list(char *word, t_cmd_list *command_line);
+void		ft_cmdlstadd_back(t_cmd_list *alst, t_node *new);
 t_cmd_list	*ft_cmdlstlast(t_cmd_list *lst);
 #endif
