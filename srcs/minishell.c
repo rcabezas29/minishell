46fd3b9f92f7f44a658170fd3f8c49ab465985b2
Fernando Ballesteros santamaria <fballest@c2r2s5.42madrid.com>
2021-11-17 14:11:35 +0100

    preparseo double free

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fballest <fballest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 13:08:30 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/11/16 19:36:14 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/11/17 14:00:41 by fballest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,17 +66,146 @@ void	reset_values(t_cmd_info *cmd_info)
 	cmd_info->return_code = 0;
 }
 
-int	check_prompt(char **prompt)
+static char 	*expand_mayorminor(char *prompt, int *i, int *j)
 {
-	int	len;
+	char	*tmp;
+	int		z;
 
-	len = ft_strlen(*prompt);
-	if ((*prompt)[len - 1] == '|' || (*prompt)[len - 1] == '>' || (*prompt)[len - 1] == '<')
+	z = 0;
+	tmp = ft_strdup(prompt);
+	while (prompt[(*i)])
+	{
+		if (prompt[(*i)] == '>' && prompt[(*i) + 1] == '>'
+			&& (prompt[(*i) + 2] != ' ' && prompt[(*i) - 1] != ' '))
+			z = 2;
+		else if (prompt[(*i)] == '<' && prompt[(*i) + 1] == '<'
+			&& (prompt[(*i) + 2] != ' ' && prompt[(*i) - 1] != ' '))
+			z = 2;
+		else if (prompt[(*i)] == '>'
+			&& (prompt[(*i) + 1] != ' ' || prompt[(*i) - 1] != ' '))
+			z = 1;
+		else if (prompt[(*i)] == '<'
+			&& (prompt[(*i) + 1] != ' ' || prompt[(*i) - 1] != ' '))
+			z = 1;
+		else if (prompt[(*i)] == '|' && prompt[(*i) + 1] != ' ')
+			z = 1;
+		else if (prompt[(*i)] == '|' && prompt[(*i) + 1] == '|'
+			&& prompt[(*i) + 2] == '|')
+		{
+			printf("Minishell: syntax error near unexpected token");
+			return (NULL);
+		}	
+		else
+			tmp[(*j)++] = prompt[(*i)++];
+		while (z > 0)
+		{
+			if (tmp)
+				tmp = ft_realloc(tmp, (ft_strlen(tmp) + 5));
+			else
+				tmp = ft_strdup(prompt);
+			tmp[(*j)++] = ' ';
+			while (z > 0)
+			{
+				tmp[(*j)++] = prompt[(*i)];
+				z--;
+			}
+			tmp[(*j)++] = ' ';
+			(*i)++;
+			z = (*i);
+			while (prompt[z])
+				tmp[(*j)++] = prompt[z++];
+			tmp[(*j)] = '\0';
+			z = 0;
+			(*i) = (*i) + 2;
+			(*j) = (*i);
+			free (prompt);
+			prompt = ft_strdup(tmp);
+			return (prompt);
+		}
+	}
+	free (prompt);
+	prompt = ft_strdup(tmp);
+	return (prompt);
+}
+
+int		check_end_prompt(char *prompt, t_cmd_info *cmd_info)
+{
+	int		len;
+
+	len = ft_strlen(prompt);
+	if (prompt[len - 1] == '|' || prompt[len - 1] == '>'
+		|| prompt[len - 1] == '<')
 	{
 		printf("minishell: syntax error near unexpected token `newline'\n");
-		return (0);
+		return (cmd_info->return_code = 1);
 	}
-	return (1);
+	return (0);
+}
+
+char	*check_prompt(char *prompt, t_cmd_info *cmd_info)
+{
+	char	*aux;
+	int		s_quotes;
+	int		d_quotes;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	s_quotes = 0;
+	d_quotes = 0;
+	if (check_end_prompt(prompt, cmd_info) == 1)
+		return (NULL);
+	aux = ft_strdup(prompt);
+	while (prompt[i])
+	{
+		if (prompt[i] == '\'' && (d_quotes == 0 || !(d_quotes % 2)))
+		{
+			if (prompt[i + 1] != '\'')
+			{
+				s_quotes += 1;
+				aux[j++] = prompt[i++];
+			}
+			else
+				i += 2;
+		}
+		else if (prompt[i] == '\"' && (s_quotes == 0 || !(s_quotes % 2)))
+		{
+			if (prompt[i + 1] != '\"')
+			{
+				d_quotes += 1;
+				aux[j++] = prompt[i++];
+			}
+			else
+				i += 2;
+		}
+		else if ((prompt[i] == '>' || prompt[i] == '<')
+			&& ((d_quotes == 0 || (d_quotes % 2) == 0)
+				&& (s_quotes == 0 || (s_quotes % 2) == 0)))
+		{
+			prompt = expand_mayorminor(prompt, &i, &j);
+			aux = ft_realloc(prompt, i + 1);
+			if (!prompt)
+			{
+				cmd_info->return_code = 1;
+				return (NULL);
+			}
+		}
+		else
+		{
+			aux[j++] = prompt[i++];
+			aux[j] = '\0';
+		}
+	}
+	aux[j] = '\0';
+	prompt = ft_realloc(aux, ft_strlen(aux));
+	if ((d_quotes > 0 && (d_quotes % 2)) || (s_quotes > 0 && (s_quotes % 2)))
+	{
+		printf("Minishell: systax error\n");
+		cmd_info->return_code = 3;
+		return (NULL);
+	}
+	return (prompt);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -93,13 +222,14 @@ int	main(int argc, char **argv, char **envp)
 	{
 		if (prompt[0] != '\0')
 		{
-			if (!check_prompt(&prompt))
+			add_history(prompt);
+			prompt = check_prompt(prompt, cmd_info);
+			if (!prompt)
 			{
 				free(prompt);
 				prompt = memory_main(argc, argv);
 				continue ;
 			}
-			add_history(prompt);
 			lexer(env, cmd_info, prompt);
 			analyze_prompt(cmd_info, env);
 			parser(cmd_info);
