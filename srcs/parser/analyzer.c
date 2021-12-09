a1b19c73f4c50a528290f64a2daca57e7fecaa24
Raul Cabezas gonzalez <rcabezas@c3r12s6.42madrid.com>
2021-12-09 15:07:34 +0100

    Passed norminette

--- srcs/parser/analyzer.c
+++ srcs/parser/analyzer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   analyzer.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fballest <fballest@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 17:32:58 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/12/08 11:17:14 by fballest         ###   ########.fr       */
+/*   Updated: 2021/12/09 15:07:03 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,6 @@ void	analyze_prompt(t_cmd_info *cmd_info, t_env *env)
 	t_type	type;
 
 	aux = cmd_info->command_list;
-	if (aux && ((t_node *)aux->content)->types == 1)
-	{
-		write(2, "syntax error near unexpected token `|'\n", 40);
-		cmd_info->return_code = 258;
-	}
 	while (aux)
 	{
 		if (((t_node *)aux->content)->types > 1
@@ -33,8 +28,7 @@ void	analyze_prompt(t_cmd_info *cmd_info, t_env *env)
 				aux = aux->next;
 			if (((t_node *)aux->content)->types == PIPE)
 			{
-				ft_putstr_fd(
-					"minishell: syntax error near unexpected token\n", 2);
+				write(2, "minishell: syntax error near unexpected token\n", 47);
 				cmd_info->return_code = 258;
 				return ;
 			}
